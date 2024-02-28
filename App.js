4f4678b45f956e26adfed5034655112d73bd379a
ebrardev <16155052@mersin.edu.tr>
2024-02-28 10:38:50 +0300

    nav config

--- App.js
+++ App.js
@@ -6,15 +6,48 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import { GlobalStyles } from './constants/style';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return(
-  <BottomTabs.Navigator>
-<BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
-<BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+  <BottomTabs.Navigator
+  screenOptions={{
+    headerStyle:{
+      backgroundColor: GlobalStyles.colors.primary500,
+   },
+   headerTintColor: "white",
+   tabBarStyle : {
+      backgroundColor: GlobalStyles.colors.primary500,
+ 
+   },
+    tabBarActiveTintColor: GlobalStyles.colors.accent500,
+  }}
+  >
+<BottomTabs.Screen name="RecentExpenses" component={RecentExpenses}
+options={ {
+  title : "Recent Expenses",
+  tabBarLabel: "Recent",
+  tabBarIcon: ({color,size}) =>
+
+
+  <FontAwesome5 name="receipt" size={size} color={color} />
+}}
+/>
+<BottomTabs.Screen name="AllExpenses"
+ component={AllExpenses}
+ options={ {
+  title : "All Expenses",
+  tabBarLabel: "All",
+  tabBarIcon: ({color,size}) =>
+
+<FontAwesome name="calendar" size={size} color={color} />
+}}
+ />
   </BottomTabs.Navigator>
   );
 }
@@ -24,7 +57,7 @@ export default function App() {
   <StatusBar style="auto" />
   <NavigationContainer>
     <Stack.Navigator>
-    <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+    <Stack.Screen name="ExpensesOverview" component={ExpensesOverview}  options={{headerShown:false}}/>
       <Stack.Screen name="ManageExpense" component={ManageExpense} />
 
 

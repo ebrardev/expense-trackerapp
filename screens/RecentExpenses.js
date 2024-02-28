6071acae675c9b892f284f26a94f6f494c1a9f45
ebrardev <16155052@mersin.edu.tr>
2024-02-28 09:24:21 +0300

    setup bottom

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,4 +1,10 @@
+ import { View, Text } from 'react-native';
 function RecentExpenses() {
 
+    return (
+        <View >
+        <Text>Recent Expenses</Text>
+        </View>
+    );
 }
 export default RecentExpenses;

4ff2c32ab8526bbd1a498ebd126d104216547942
ebrardev <16155052@mersin.edu.tr>
2024-03-05 12:13:18 +0300

    Add useEffect hook to fetch expenses from backend

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,20 @@
 
-import { useContext } from 'react';
+import { useContext ,useEffect} from 'react';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/Date';
+import { FetchExpenses } from '../util/Http';
 function RecentExpenses() {
 
+
  const ExpensesCtx= useContext(ExpensesContext)
+
+ useEffect(()=>{
+  async function getExpenses() {
+    const expenses= await FetchExpenses()
+  }
+  getExpenses()
+ },[])
   const recentExpenses = ExpensesCtx.expenses.filter(expense => {
     const today = new Date();
     const date7DaysAgo= getDateMinusDays(today,7)
--- util/Http.js
+++ util/Http.js
@@ -1,6 +1,27 @@
 import axios from 'axios';
 
+const BACKEND_URL = 'https://expensetracker-ab46f-default-rtdb.firebaseio.com/';
  export function StoreExpense(expenseData) {
-    axios.post('https://expensetracker-ab46f-default-rtdb.firebaseio.com/expenses.json', expenseData)
+    axios.post( BACKEND_URL +'/expenses.json', expenseData)
 expenseData
+}
+
+export  async function FetchExpenses() {
+   const response = await  axios.get( BACKEND_URL +'/expenses.json')
+
+   const expenses = []
+
+
+   con
+    for (const key in response.data) {
+       const expenseObj = {
+            id: key,
+            amount: response.data[key].amount,
+            date: new Date( response.data[key].date),
+            description: response.data[key].description
+       }
+         expenses.push(expenseObj)
+    }
+    return expenses
+
 }
